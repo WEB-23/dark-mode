0ed5bce82ca58fb292d57cf9b984351d0c6a5b87
andrew15722 <andrew15722@yahoo.com>
2019-10-02 14:33:38 -0700

    finished up the useDarmMode hook and got it working the application, met mvp goals

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import useLocalStorage from './useLocalStorage.js';
+
+const useDarkMode = () => {
+	const [ isDark, setIsDark ] = useLocalStorage('true');
+
+	useEffect(
+		() => {
+			isDark ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
+		},
+		[ isDark ]
+	);
+	return [ isDark, setIsDark ];
+};
+
+export default useDarkMode;
